09821679c83731f578c698700e6cc809fd18f11a
Max Bruckner <max@maxbruckner.de>
2017-05-26 01:46:54 +0200

    Main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::{thread,time};
+
 fn main() {
-    println!("Hello, world!");
+    let sleep_time = time::Duration::from_secs(1);
+    loop {
+        println!("Hello, world!");
+        thread::sleep(sleep_time);
+    }
 }
