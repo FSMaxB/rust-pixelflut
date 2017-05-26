8f0f8363d95588cc1bfeccf944ab70b3ad7fcf75
Max Bruckner <max@maxbruckner.de>
2017-05-26 02:42:59 +0200

    Actually send pixels to pixelflut

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,24 @@ fn write_to_stream(line: &String, stream: &mut TcpStream) -> bool {
     return true;
 }
 
+fn pixel(x: u16, y: u16, red: u8, green: u8, blue: u8) -> String {
+    return format!("PX {} {} {:02x}{:02x}{:02x}", x, y, red, green, blue);
+}
+
 fn main() {
     let sleep_time = time::Duration::from_secs(1);
-    let tcp_option = TcpStream::connect("localhost:8080");
+    let tcp_option = TcpStream::connect("94.45.234.7:1234");
     if !tcp_option.is_ok() {
         println!("Failed to open TCP stream.");
         exit(1);
     }
     let mut tcp_stream = tcp_option.unwrap();
-    let mut counter = 0;
 
     loop {
-        let number = format!("{}", counter);
-        write_to_stream(&number, &mut tcp_stream);
-        counter = counter + 1;
-        thread::sleep(sleep_time);
+        for x in 0..1920 {
+            for y in 0..1080 {
+                write_to_stream(&pixel(x, y, 0, 255, 255), &mut tcp_stream);
+            }
+        }
     }
 }
